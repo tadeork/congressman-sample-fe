935f2c7444513d9f646b34a291b13d529fb15e69
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2021-06-09 20:24:02 -0300

    header interceptor

---
+++ src/app/interceptors/header.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_KEY, CONGRESS_API_URL } from '../constants/APIEndpoints';
+
+@Injectable()
+export class HeaderInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (!request.url.includes(CONGRESS_API_URL)) {
+      return next.handle(request);
+    }
+
+    const modified = request.clone({
+      setHeaders: { 'X-API-Key': API_KEY },
+    });
+    
+    return next.handle(modified);
+  }
+}
